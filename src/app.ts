7bc52bfc1c449eaf88ec4fd92802d1b75ba67631
Shiv Pratap <sp09singhwaghel@gmail.com>
2024-04-26 14:17:26 +0530

    add : setup express server

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express from "express"
+import { Request, Response, NextFunction } from "express"
+
+export const app = express()
+
+// ROUTES
+
+app.get('/',(req:Request,res:Response,next:NextFunction)=>{
+    
+})
